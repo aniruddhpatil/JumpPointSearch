b4746bb1b8bbf138c2d1e38c51be0b72e2d1dff4
Rocket_Chat <naidu.adarsh2511@gmail.com>
2020-06-21 17:32:40 +0530

    heuristics for jump point search

---
+++ src/algos/JPS/jps.py
@@ -0,0 +1,12 @@
+import math
+
+def heuristic(a, b, hchoice):
+    if hchoice == 1:
+        xdist = math.fabs(b[0] - a[0])
+        ydist = math.fabs(b[1] - a[1])
+        if xdist > ydist:
+            return 14 * ydist + 10 * (xdist - ydist)
+        else:
+            return 14 * xdist + 10 * (ydist - xdist)
+    if hchoice == 2:
+        return math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
