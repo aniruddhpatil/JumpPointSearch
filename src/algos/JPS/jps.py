c221fa9aa1f5b414317c0ac4096966bf8411fe7c
Rocket_Chat <naidu.adarsh2511@gmail.com>
2020-06-21 17:36:30 +0530

    jump point search graph setup

--- src/algos/JPS/jps.py
+++ src/algos/JPS/jps.py
@@ -37,3 +37,71 @@ def dblock(cX, cY, dX, dY, matrix):
         return True
     else:
         return False
+
+def direction(cX, cY, pX, pY):
+    dX = int(math.copysign(1, cX - pX))
+    dY = int(math.copysign(1, cY - pY))
+    if cX - pX == 0:
+        dX = 0
+    if cY - pY == 0:
+        dY = 0
+    return (dX, dY)
+
+
+def nodeNeighbours(cX, cY, parent, matrix):
+    neighbours = []
+    if type(parent) != tuple:
+        for i, j in [
+            (-1, 0),
+            (0, -1),
+            (1, 0),
+            (0, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, -1),
+            (1, 1),
+        ]:
+            if not blocked(cX, cY, i, j, matrix):
+                neighbours.append((cX + i, cY + j))
+
+        return neighbours
+    dX, dY = direction(cX, cY, parent[0], parent[1])
+
+    if dX != 0 and dY != 0:
+        if not blocked(cX, cY, 0, dY, matrix):
+            neighbours.append((cX, cY + dY))
+        if not blocked(cX, cY, dX, 0, matrix):
+            neighbours.append((cX + dX, cY))
+        if (
+            not blocked(cX, cY, 0, dY, matrix)
+            or not blocked(cX, cY, dX, 0, matrix)
+        ) and not blocked(cX, cY, dX, dY, matrix):
+            neighbours.append((cX + dX, cY + dY))
+        if blocked(cX, cY, -dX, 0, matrix) and not blocked(
+            cX, cY, 0, dY, matrix
+        ):
+            neighbours.append((cX - dX, cY + dY))
+        if blocked(cX, cY, 0, -dY, matrix) and not blocked(
+            cX, cY, dX, 0, matrix
+        ):
+            neighbours.append((cX + dX, cY - dY))
+
+    else:
+        if dX == 0:
+            if not blocked(cX, cY, dX, 0, matrix):
+                if not blocked(cX, cY, 0, dY, matrix):
+                    neighbours.append((cX, cY + dY))
+                if blocked(cX, cY, 1, 0, matrix):
+                    neighbours.append((cX + 1, cY + dY))
+                if blocked(cX, cY, -1, 0, matrix):
+                    neighbours.append((cX - 1, cY + dY))
+
+        else:
+            if not blocked(cX, cY, dX, 0, matrix):
+                if not blocked(cX, cY, dX, 0, matrix):
+                    neighbours.append((cX + dX, cY))
+                if blocked(cX, cY, 0, 1, matrix):
+                    neighbours.append((cX + dX, cY + 1))
+                if blocked(cX, cY, 0, -1, matrix):
+                    neighbours.append((cX + dX, cY - 1))
+    return neighbours
